656d34169a7630e580f486831be2628c7e7d4d84
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-03-15 13:21:56 +0100

    Remove envelope code

--- src/models/wikimedia/wikipedia/templates/wikipedia_page_reference.py
+++ src/models/wikimedia/wikipedia/templates/wikipedia_page_reference.py
@@ -382,32 +382,6 @@ def time_validator(cls, v):
     return date
 
 
-class BaseSchema(Schema):
-    # Custom options
-    __envelope__ = {"single": None, "many": None}
-    __model__ = WikipediaPageReference
-
-    def get_envelope_key(self, many):
-        """Helper to get the envelope key."""
-        key = self.__envelope__["many"] if many else self.__envelope__["single"]
-        assert key is not None, "Envelope key undefined"
-        return key
-
-    @pre_load(pass_many=True)
-    def unwrap_envelope(self, data, many, **kwargs):
-        key = self.get_envelope_key(many)
-        return data[key]
-
-    @post_dump(pass_many=True)
-    def wrap_with_envelope(self, data, many, **kwargs):
-        key = self.get_envelope_key(many)
-        return {key: data}
-
-    @post_load
-    def make_object(self, data, **kwargs):
-        return self.__model__(**data)
-
-
 class WikipediaPageReferenceSchema(Schema):
     """Marshmellow schema to validate the templates
     We declare the validators on the attributes because the
