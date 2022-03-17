00a5c5213f66d48993adab871b6cd1ca54c6b6df
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-03-17 14:43:47 +0100

    Test cite journal also

--- tests/test_english_wikipedia_page_reference.py
+++ tests/test_english_wikipedia_page_reference.py
@@ -34,7 +34,7 @@ class TestEnglishWikipediaPageReferenceSchema(TestCase):
         reference = EnglishWikipediaPageReferenceSchema().load(data)
         console.print(reference)
 
-    def test_parse_persons(self):
+    def test_parse_persons_from_cite_web(self):
         data = {
             "url": "https://www.stereogum.com/1345401/turntable-interview/interviews/",
             "title": "Turntable Interview: !!!",
@@ -54,3 +54,26 @@ class TestEnglishWikipediaPageReferenceSchema(TestCase):
         person = reference.persons_without_role[0]
         assert person.given == "Melissa"
         assert person.surname == "Locker"
+
+    def test_parse_persons_from_cite_journal(self):
+        data = {
+            "last1": "Skaaning",
+            "first1": "Svend-Erik",
+            "title": "Different Types of Data and the Validity of Democracy Measures",
+            "journal": "Politics and Governance",
+            "volume": "6",
+            "issue": "1",
+            "page": "105",
+            "doi": "10.17645/pag.v6i1.1183",
+            "year": "2018",
+            "doi_access": "free",
+            "template_name": "cite journal",
+        }
+        reference: EnglishWikipediaPageReference = (
+            EnglishWikipediaPageReferenceSchema().load(data)
+        )
+        reference.parse_persons()
+        console.print(reference)
+        person = reference.persons_without_role[0]
+        assert person.given == "Svend-Erik"
+        assert person.surname == "Skaaning"
