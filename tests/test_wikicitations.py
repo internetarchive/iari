d5d74a96ed34a3952776b5b70d3768fda8108ccc
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-04-05 21:27:26 +0200

    test_wikicitations.py: test_prepare_and_upload_reference_item(): Fix test so that it now passes!

--- tests/test_wikicitations.py
+++ tests/test_wikicitations.py
@@ -2,6 +2,8 @@ import logging
 from unittest import TestCase
 
 import config
+from src import console
+from src.models.wikicitations import WCDProperty
 from src.models.wikimedia.wikipedia.templates.english_wikipedia_page_reference import (
     EnglishWikipediaPageReference,
 )
@@ -18,15 +20,34 @@ class TestWikiCitations(TestCase):
         wc = WikiCitations()
         wppage = WikipediaPage()
         wppage.__get_wikipedia_page_from_title__(title="Democracy")
-        reference = wc.prepare_and_upload_reference_item(
-            page_reference=EnglishWikipediaPageReference(
-                title="test", template_name="test", doi="test"
-            ),
-            wikipedia_page=wppage,
+        reference = EnglishWikipediaPageReference(
+            **{
+                "last": "Tangian",
+                "first": "Andranik",
+                "date": "2020",
+                "title": "Analytical Theory of Democracy: History, Mathematics and Applications",
+                "series": "Studies in Choice and Welfare",
+                "publisher": "Springer",
+                "location": "Cham, Switzerland",
+                "isbn": "978-3-030-39690-9",
+                "doi": "10.1007/978-3-030-39691-6",
+                "s2cid": "216190330",
+                "template_name": "cite book",
+            }
         )
-        logger.info(f"url: {reference.wikicitations_url}")
-
-        wppage = wc.prepare_and_upload_wikipedia_page_item(
-            wikipedia_page=wppage,
+        reference.parse_persons()
+        reference.generate_hash()
+        assert len(reference.persons_without_role) > 0
+        item = wc.__prepare_new_reference_item__(
+            page_reference=reference, wikipedia_page=wppage
         )
-        logger.info(f"url: {wppage.wikicitations_url}")
+        console.print(item.get_json())
+        assert (
+            item.claims.get(property=WCDProperty.AUTHOR_NAME_STRING.value) is not None
+        )
+        # logger.info(f"url: {reference.wikicitations_url}")
+        #
+        # wppage = wc.prepare_and_upload_wikipedia_page_item(
+        #     wikipedia_page=wppage,
+        # )
+        # logger.info(f"url: {wppage.wikicitations_url}")
