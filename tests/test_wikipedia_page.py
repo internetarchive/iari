3f95559d8053e41ca441870c8ee101d3a523777e
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-04-23 15:14:58 +0200

    test_wikipedia_page.py: test_fix_dash(): New test

--- tests/test_wikipedia_page.py
+++ tests/test_wikipedia_page.py
@@ -1,7 +1,10 @@
 import logging
 from unittest import TestCase
 
+import pywikibot
+
 import config
+from src import WikipediaPage, WikimediaSite, console
 
 logging.basicConfig(level=config.loglevel)
 logger = logging.getLogger(__name__)
@@ -40,3 +43,22 @@ class TestWikipediaPage(TestCase):
     # def test___parse_templates__(self):
     #     pass
     pass
+
+    def test_fix_dash(self):
+        site = pywikibot.Site(code="en", fam=WikimediaSite.WIKIPEDIA.value)
+        page = WikipediaPage(
+            pywikibot_site=site,
+            language_code="en",
+            wikimedia_site=WikimediaSite.WIKIPEDIA,
+        )
+        page.__get_wikipedia_page_from_title__(title="Easter Island")
+        page.__extract_and_parse_references__()
+        logger.info(f"{len(page.references)} references found")
+        for ref in page.references:
+            if config.loglevel == logging.INFO or config.loglevel == logging.DEBUG:
+                # console.print(ref.template_name)
+                if (
+                    ref.url
+                    == "http://www.ine.cl/canales/chile_estadistico/censos_poblacion_vivienda/censo_pobl_vivi.php"
+                ):
+                    console.print(ref.url, ref.archive_url)
