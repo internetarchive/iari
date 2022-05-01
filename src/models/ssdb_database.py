037f71e5bffb37e0905961e4382f97f51da7addf
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-01 11:52:09 +0200

    ssdb_database.py: Default to the AWS SSDB and document why we are not using the auth command.

--- src/models/ssdb_database.py
+++ src/models/ssdb_database.py
@@ -8,13 +8,20 @@ logger = logging.getLogger(__name__)
 
 
 class SsdbDatabase(BaseModel):
-    password: str = "password"
+    """Class modeling the SSDB database
+
+    SSDB has an auth command for authentication but
+    we don't use it because it is sent in clear text and that is pretty useless"""
+
     port: int = 8888  # "6379"
-    host: str = "127.0.0.1"
+    host: str = "18.217.22.248"
     connection: Optional[Any]
 
     def connect(self):
-        self.connection = pyssdb.Client(host=self.host, port=self.port)
+        self.connection = pyssdb.Client(
+            host=self.host,
+            port=self.port,
+        )
 
     def get_info(self):
         return self.connection.info()
