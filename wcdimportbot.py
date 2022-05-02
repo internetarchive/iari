11328e0dec631b0d10aab416767867efed354345
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-02 13:46:23 +0200

    wcdimportbot.py: Catch ModuleNotFoundError and tell the user to follow the instructions in the readme.

--- wcdimportbot.py
+++ wcdimportbot.py
@@ -1,6 +1,11 @@
 import logging
 
-import config
+try:
+    import config
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "config.py now found. Please follow the instructions in the README about how to set up the config.py file"
+    )
 from src import WcdImportBot
 
 logging.basicConfig(level=config.loglevel)
