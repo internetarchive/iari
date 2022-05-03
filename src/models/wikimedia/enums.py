02236f37d925b3655c0b1e177a4c817ed419fd3d
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-03 09:15:55 +0200

    enums.py: Comment out unused ones.

--- src/models/wikimedia/enums.py
+++ src/models/wikimedia/enums.py
@@ -10,38 +10,38 @@ from enum import Enum
 #     CROSSREF = "Q5188229"
 
 
-class WikidataNamespaceLetters(Enum):
-    PROPERTY = "P"
-    ITEM = "Q"
-    LEXEME = "L"
-    # FORM = "F"
-    # SENSE = "S"
+# class WikidataNamespaceLetters(Enum):
+#     PROPERTY = "P"
+#     ITEM = "Q"
+#     LEXEME = "L"
+#     # FORM = "F"
+#     # SENSE = "S"
 
 
-class WikimediaLanguage(Enum):
-    DANISH = "Q9035"
-    SWEDISH = "Q9027"
-    BOKMÅL = "Q25167"
-    ENGLISH = "Q1860"
-    FRENCH = "Q150"
-    RUSSIAN = "Q7737"
-    ESTONIAN = "Q9072"
-    MALAYALAM = "Q36236"
-    LATIN = "Q397"
-    HEBREW = "Q9288"
-    BASQUE = "Q8752"
-    GERMAN = "Q188"
-    BENGALI = "Q9610"
-    CZECH = "Q9056"
+# class WikimediaLanguage(Enum):
+#     DANISH = "Q9035"
+#     SWEDISH = "Q9027"
+#     BOKMAL = "Q25167"
+#     ENGLISH = "Q1860"
+#     FRENCH = "Q150"
+#     RUSSIAN = "Q7737"
+#     ESTONIAN = "Q9072"
+#     MALAYALAM = "Q36236"
+#     LATIN = "Q397"
+#     HEBREW = "Q9288"
+#     BASQUE = "Q8752"
+#     GERMAN = "Q188"
+#     BENGALI = "Q9610"
+#     CZECH = "Q9056"
 
 
 class WikimediaSite(Enum):
     WIKIPEDIA = "wikipedia"
 
 
-class WikimediaEditType(Enum):
-    NEW = "new"
-    EDIT = "edit"
-    LOG = "log"
-    CATEGORIZE = "categorize"
-    UNKNOWN = "142"
+# class WikimediaEditType(Enum):
+#     NEW = "new"
+#     EDIT = "edit"
+#     LOG = "log"
+#     CATEGORIZE = "categorize"
+#     UNKNOWN = "142"
