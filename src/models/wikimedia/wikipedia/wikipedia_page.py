92ee7310a10195ce8d202a1b829ddb9a2afd8578
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-05 00:07:55 +0200

    Try silencing pywikibot logging

--- src/models/wikimedia/wikipedia/wikipedia_page.py
+++ src/models/wikimedia/wikipedia/wikipedia_page.py
@@ -21,6 +21,8 @@ from src.models.wikimedia.wikipedia.templates.wikipedia_page_reference import (
 )
 
 logger = logging.getLogger(__name__)
+pywikibot_logger = logging.getLogger("pywiki")
+pywikibot_logger.setLevel(config.loglevel)
 
 
 class WikipediaPage(BaseModel):
