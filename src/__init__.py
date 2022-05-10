cda1862df220bfbc33ddd06a2441f0060132c353
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-10 18:11:12 +0200

    __init__.py: WcdImportBot: delete_one_page(): Improve error message

--- src/__init__.py
+++ src/__init__.py
@@ -137,7 +137,10 @@ class WcdImportBot(BaseModel):
                 else:
                     raise ValueError("md5hash was None")
             else:
-                raise ValueError("got no item id from the cache")
+                if config.use_cache:
+                    raise ValueError("got no item id from the cache")
+                else:
+                    raise ValueError("got no item id from sparql")
 
     def extract_and_upload_all_pages_to_wikicitations(self):
         [page.extract_and_upload_to_wikicitations() for page in self.pages]
