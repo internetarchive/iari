d199f72bd1e06b2416e63557d359cfc4c24881e1
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-05-24 08:41:14 +0200

    test_person.py: test_full_name(): Add assertions for space, tab and newline.

--- tests/test_person.py
+++ tests/test_person.py
@@ -15,6 +15,12 @@ class TestPerson(TestCase):
         assert p2.full_name == "testtest"
         p3 = Person(name_string="")
         assert p3.full_name == ""
+        p4 = Person(name_string=" ", given=" ", surname=" ")
+        assert p4.full_name == ""
+        p5 = Person(name_string="\t", given="\t", surname="\t")
+        assert p5.full_name == ""
+        p6 = Person(name_string="\n", given="\n", surname="\n")
+        assert p6.full_name == ""
 
     def test_has_number(self):
         p = Person(given="test", surname="test")
