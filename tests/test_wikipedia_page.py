ddfcb9827368c85f943dc2bdfbc13ba8f580c556
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-07-11 23:51:57 +0200

    test_wikipedia_page.py: Remove comment

--- tests/test_wikipedia_page.py
+++ tests/test_wikipedia_page.py
@@ -15,39 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 class TestWikipediaPage(TestCase):
-    # def test_extract_references(self):
-    #     site = pywikibot.Site(code="en", fam=WikimediaSite.WIKIPEDIA.value)
-    #     page = WikipediaPage(
-    #         title="Anarchism",
-    #         pywikibot_site=site,
-    #         language_code="en",
-    #         wikimedia_site=WikimediaSite.WIKIPEDIA,
-    #     )
-    #     page.__extract_references__()
-    #     logger.info(len(page.references))
-    #     for ref in page.references:
-    #         if config.loglevel == logging.INFO or config.loglevel == logging.DEBUG:
-    #             console.print(ref.template_name)
-    #             # console.print(ref.dict())
-    #     assert len(page.references) >= 134
-    #     page = WikipediaPage(
-    #         title="Democracy",
-    #         pywikibot_site=site,
-    #         language_code="en",
-    #         wikimedia_site=WikimediaSite.WIKIPEDIA,
-    #     )
-    #     page.__extract_references__()
-    #     logger.info(len(page.references))
-    #     for ref in page.references:
-    #         if config.loglevel == logging.INFO or config.loglevel == logging.DEBUG:
-    #             console.print(ref.template_name)
-    #     assert len(page.references) >= 216
-    #     # self.fail()
-    #
-    # def test___parse_templates__(self):
-    #     pass
-    pass
-
     def test_fix_dash(self):
         from src.models.wikimedia.wikipedia.wikipedia_page import WikipediaPage
 
