980076addf5b7c3902af17da54da46b53c638b9a
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-07-12 00:58:19 +0200

    test_wikipedia_page.py: test_get_wcdqid_from_hash_via_sparql(): Update to changes in model. test_compare_data_and_update_additional_reference(), test_compare_data_and_update_removed_reference(), test_compare_and_update_page(): New test.

--- tests/test_wikipedia_page.py
+++ tests/test_wikipedia_page.py
@@ -6,9 +6,13 @@ from unittest import TestCase
 import pytest
 
 import config
+from src import WcdImportBot
 from src.helpers import console
 from src.models.wikibase.sandbox_wikibase import SandboxWikibase
 from src.models.wikimedia.enums import WikimediaSite
+from src.models.wikimedia.wikipedia.templates.english_wikipedia_page_reference import (
+    EnglishWikipediaPageReference,
+)
 
 logging.basicConfig(level=config.loglevel)
 logger = logging.getLogger(__name__)
@@ -58,8 +62,8 @@ class TestWikipediaPage(TestCase):
             title="Test",
         )
         # page.__fetch_page_data__(title="Test")
-        page.extract_and_upload_to_wikicitations()
-        wcdqid = page.wikicitations_qid
+        page.extract_and_upload_to_wikibase()
+        wcdqid = page.wikibase_return.item_qid
         console.print(
             f"Waiting {config.sparql_sync_waiting_time_in_seconds} seconds for WCDQS to sync"
         )
@@ -67,3 +71,69 @@ class TestWikipediaPage(TestCase):
         check_wcdqid = page.__get_wcdqid_from_hash_via_sparql__(md5hash=page.md5hash)
         print(wcdqid, check_wcdqid)
         assert wcdqid == check_wcdqid
+
+    def test_compare_data_and_update_additional_reference(self):
+        """First delete the test page, then upload it with one reference.
+        Then"""
+        from src.models.wikimedia.wikipedia.wikipedia_page import WikipediaPage
+
+        bot = WcdImportBot(wikibase=SandboxWikibase())
+        bot.delete_one_page(title="Test")
+        # exit()
+        data = dict(
+            url="https://archive.org/details/catalogueofshipw0000wils/",
+            template_name="cite book",
+        )
+        reference = EnglishWikipediaPageReference(**data)
+        reference.wikibase = SandboxWikibase()
+        reference.finish_parsing_and_generate_hash()
+        wp = WikipediaPage(title="Test", wikibase=SandboxWikibase())
+        wp.__fetch_page_data__()
+        wp.__generate_hash__()
+        wp.references.append(reference)
+        wp.__import_page_and_references__()
+        # press_enter_to_continue()
+        wp2 = WikipediaPage(title="Test", wikibase=SandboxWikibase())
+        wp2.__fetch_page_data__()
+        wp2.__generate_hash__()
+        wp2.references.append(reference)
+        data2 = dict(
+            title="World War I",
+            url="https://books.google.ca/books?id=on0TaPqFXbcC&pg=PA431",
+            template_name="cite book",
+        )
+        reference2 = EnglishWikipediaPageReference(**data2)
+        reference2.wikibase = SandboxWikibase()
+        reference2.finish_parsing_and_generate_hash()
+        wp2.references.append(reference2)
+        console.print(wp2.references)
+        wp2.extract_and_upload_to_wikibase()
+
+    def test_compare_data_and_update_removed_reference(self):
+        from src.models.wikimedia.wikipedia.wikipedia_page import WikipediaPage
+
+        bot = WcdImportBot(wikibase=SandboxWikibase())
+        bot.delete_one_page(title="Test")
+
+        data = dict(
+            url="https://archive.org/details/catalogueofshipw0000wils/",
+            template_name="cite book",
+        )
+        reference = EnglishWikipediaPageReference(**data)
+        reference.wikibase = SandboxWikibase()
+        reference.finish_parsing_and_generate_hash()
+        wp = WikipediaPage(title="Test", wikibase=SandboxWikibase())
+        wp.__fetch_page_data__()
+        wp.__generate_hash__()
+        wp.references.append(reference)
+        wp.__import_page_and_references__()
+        # press_enter_to_continue()
+        wp = WikipediaPage(title="Test", wikibase=SandboxWikibase())
+        wp.__fetch_page_data__()
+        wp.__generate_hash__()
+        wp.extract_and_upload_to_wikibase()
+
+    def test_compare_and_update_page(self):
+        # TODO upload a test page with some statement
+        # then update it with a page with one more statement
+        pass
