9501011a3c8ff9db20f3b69db28abbd8f1d6b239
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-07-13 17:31:38 +0200

    update.py: Improve UI and switch more messages to print.

--- src/models/wikibase/crud/update.py
+++ src/models/wikibase/crud/update.py
@@ -9,6 +9,7 @@ from wikibaseintegrator.entities import ItemEntity  # type: ignore
 from wikibaseintegrator.models import Claim  # type: ignore
 from wikibaseintegrator.wbi_enums import ActionIfExists  # type: ignore
 
+from src import console
 from src.models.exceptions import MissingInformationError
 from src.models.wikibase.crud import WikibaseCrud
 from src.models.wikibase.crud.read import WikibaseCrudRead
@@ -35,79 +36,84 @@ class WikibaseCrudUpdate(WikibaseCrud):
         testing: bool = False,
     ) -> List[Claim]:
         """We compare claims one by one and update the item in the end"""
-        from src.models.wikimedia.wikipedia.wikipedia_page import WikipediaPage
+        with console.status("Updating claims and uploading to Wikibase..."):
+            from src.models.wikimedia.wikipedia.wikipedia_page import WikipediaPage
 
-        if isinstance(entity, WikipediaPage):
-            # We remove these because we want to update them easily
-            # Another and harder way is to compare all values and
-            # deprecate the ones that are now gone.
-            # This collides with the following user story:
-            # "as a user I want to know if a reference has disappeared from the page and when"
-            logger.debug("__compare_claims__: deleting all reference claims")
-            wikibase_item.claims.remove(property=self.wikibase.CITATIONS)
-            wikibase_item.claims.remove(property=self.wikibase.STRING_CITATIONS)
-        claims_to_be_added = []
-        property_numbers = {
-            claim.mainsnak.property_number for claim in wikibase_item.claims
-        }
-        logger.debug(f"Found these property numbers {property_numbers}")
-        for claim in new_item.claims:
-            # For now we only update statements that are completely missing
-            # We thus do not add/remove qualifiers or references
-            # nor update any values on existing statements
-            if claim.mainsnak.property_number not in property_numbers:
-                # For now we don't update the hash even though adding
-                # oclc, pmid, doi, isbn cause the hash to change
-                logger.debug(f"Adding missing claim {claim}")
-                wikibase_item.claims.add(claim, action_if_exists=ActionIfExists.KEEP)
-                claims_to_be_added.append(claim)
-        number_of_added_claims = len(claims_to_be_added)
-        if number_of_added_claims > 0:
-            if not testing:
-                self.__setup_wikibase_integrator_configuration__()
-                wikibase_item.write(
-                    summary=(
-                        f"Updated the item with {number_of_added_claims} more claims "
-                        "based on changes in Wikipedia"
+            if isinstance(entity, WikipediaPage):
+                # We remove these because we want to update them easily
+                # Another and harder way is to compare all values and
+                # deprecate the ones that are now gone.
+                # This collides with the following user story:
+                # "as a user I want to know if a reference has disappeared from the page and when"
+                logger.debug("__compare_claims__: deleting all reference claims")
+                wikibase_item.claims.remove(property=self.wikibase.CITATIONS)
+                wikibase_item.claims.remove(property=self.wikibase.STRING_CITATIONS)
+            claims_to_be_added = []
+            property_numbers = {
+                claim.mainsnak.property_number for claim in wikibase_item.claims
+            }
+            logger.debug(f"Found these property numbers {property_numbers}")
+            for claim in new_item.claims:
+                # For now we only update statements that are completely missing
+                # We thus do not add/remove qualifiers or references
+                # nor update any values on existing statements
+                if claim.mainsnak.property_number not in property_numbers:
+                    # For now we don't update the hash even though adding
+                    # oclc, pmid, doi, isbn cause the hash to change
+                    logger.debug(f"Adding missing claim {claim}")
+                    wikibase_item.claims.add(
+                        claim, action_if_exists=ActionIfExists.KEEP
                     )
-                )
-                logger.info(f"Added {number_of_added_claims} claims to this item")
-                return claims_to_be_added
+                    claims_to_be_added.append(claim)
+            number_of_added_claims = len(claims_to_be_added)
+            if number_of_added_claims > 0:
+                if not testing:
+                    self.__setup_wikibase_integrator_configuration__()
+                    wikibase_item.write(
+                        summary=(
+                            f"Updated the item with {number_of_added_claims} more claims "
+                            "based on changes in Wikipedia"
+                        )
+                    )
+                    console.print(
+                        f"Added {number_of_added_claims} new claims to this item"
+                    )
+                    return claims_to_be_added
+                else:
+                    logger.info(
+                        f"Found {number_of_added_claims} claims to be added to this item"
+                    )
+                    return claims_to_be_added
+            elif len(wikibase_item.claims) != len(new_item.claims):
+                if not testing:
+                    self.__setup_wikibase_integrator_configuration__()
+                    wikibase_item.write(
+                        summary=(
+                            "Removed claims "
+                            "based on changes in Wikipedia"
+                            "This is an edge case and should rarely happen. "
+                            "Example: a page had one reference and it was "
+                            "removed since last update"
+                        )
+                    )
+                    console.print(
+                        f"Updated the item with fewer claims than before. "
+                        f"This is an edge case and should rarely happen. "
+                        f"Example: a page had one reference and it was "
+                        f"removed since last update"
+                    )
+                    return []
+                else:
+                    logger.info(
+                        "Claims were removed. "
+                        "But updating was skipped because testing was True."
+                    )
+                    return []
             else:
-                logger.info(
-                    f"Found {number_of_added_claims} claims to be added to this item"
-                )
-                return claims_to_be_added
-        elif len(wikibase_item.claims) != len(new_item.claims):
-            if not testing:
-                self.__setup_wikibase_integrator_configuration__()
-                wikibase_item.write(
-                    summary=(
-                        "Removed claims "
-                        "based on changes in Wikipedia"
-                        "This is an edge case and should rarely happen. "
-                        "Example: a page had one reference and it was "
-                        "removed since last update"
-                    )
-                )
-                logger.info(
-                    f"Updated the item with fewer claims than before. "
-                    f"This is an edge case and should rarely happen. "
-                    f"Example: a page had one reference and it was "
-                    f"removed since last update"
+                console.print(
+                    f"No new claims were added so we skip updating."  # this {entity.__repr_name__()}"
                 )
                 return []
-            else:
-                logger.info(
-                    "Claims were removed. "
-                    "But updating was skipped because testing was True."
-                )
-                return []
-        else:
-            logger.info(
-                f"No new claims were added so we skip updating this {entity.__repr_name__()}"
-            )
-            return []
 
     def compare_and_update_claims(
         self,
@@ -129,6 +135,16 @@ class WikibaseCrudUpdate(WikibaseCrud):
             if isinstance(entity, WikipediaPageReference):
                 if not wikipedia_page:
                     raise MissingInformationError("wikipedia_page was None")
+                if entity.title:
+                    console.print(
+                        f"Comparing {entity.template_name} "
+                        f"reference with the title '{entity.title}'"
+                    )
+                else:
+                    console.print(
+                        f"Comparing {entity.template_name} "
+                        f"reference with missing title"
+                    )
                 logger.info(
                     f"See {self.wikibase.entity_url(item_id=entity.wikibase_return.item_qid)}"
                 )
@@ -149,7 +165,7 @@ class WikibaseCrudUpdate(WikibaseCrud):
                         "This should never happen."
                     )
             if wikibase_item.claims == new_item.claims:
-                logger.info("The claims of the items are equal. No update needed.")
+                console.print("The claims of the items are equal. No update needed.")
             else:
                 self.__compare_claims_and_upload__(
                     entity=entity, wikibase_item=wikibase_item, new_item=new_item
