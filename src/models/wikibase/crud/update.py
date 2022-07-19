792cd2f75847a227a61bfc3aceba2cae8ebbb5d8
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-07-19 12:20:54 +0200

    update.py: WikibaseCrudUpdate: __compare_claims_and_upload__(): More logging of property numbers to INFO

--- src/models/wikibase/crud/update.py
+++ src/models/wikibase/crud/update.py
@@ -53,17 +53,26 @@ class WikibaseCrudUpdate(WikibaseCrud):
                 wikibase_item.claims.remove(property=self.wikibase.STRING_CITATIONS)
             if isinstance(entity, WikipediaPageReference):
                 """This is needed to fix https://github.com/internetarchive/wcdimportbot/issues/85"""
+                logger.debug("Deleting the WEBSITE claim")
                 wikibase_item.claims.remove(property=self.wikibase.WEBSITE)
             claims_to_be_added = []
-            property_numbers = {
+            current_property_numbers = {
                 claim.mainsnak.property_number for claim in wikibase_item.claims
             }
-            logger.debug(f"Found these property numbers {property_numbers}")
+            logger.info(
+                f"Found these property numbers {current_property_numbers} in the item in Wikibase"
+            )
+            new_property_numbers = {
+                claim.mainsnak.property_number for claim in new_item.claims
+            }
+            logger.info(
+                f"Found these property numbers {new_property_numbers} on the newly prepared item"
+            )
             for claim in new_item.claims:
                 # For now we only update statements that are completely missing
                 # We thus do not add/remove qualifiers or references
                 # nor update any values on existing statements
-                if claim.mainsnak.property_number not in property_numbers:
+                if claim.mainsnak.property_number not in current_property_numbers:
                     # For now we don't update the hash even though adding
                     # oclc, pmid, doi, isbn cause the hash to change
                     logger.debug(f"Adding missing claim {claim}")
