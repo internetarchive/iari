6c441d7a572713ccfb2d882c6ef81626debd4d15
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-09-15 18:50:21 +0200

    ia_sandbox_wikibase.py: Update properties and items.

--- src/models/wikibase/ia_sandbox_wikibase.py
+++ src/models/wikibase/ia_sandbox_wikibase.py
@@ -13,79 +13,71 @@ class IASandboxWikibase(Wikibase):
     query_service_url = wikibase_url + "/query/"
 
     # Properties
-    DOI = "P46"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    GOOGLE_BOOKS_ID = "P366"  # datatype: WikibaseDatatype.EXTERNALID description: Identifier used to link books at Google.
-    INTERNET_ARCHIVE_ID = "P316"  # datatype: WikibaseDatatype.EXTERNALID description: Identifier used to link books and other resources at Internet Archive.
-    ISBN_10 = "P58"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    ISBN_13 = "P59"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    MEDIAWIKI_PAGE_ID = "P64"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    OCLC_CONTROL_NUMBER = "P474"  # datatype: WikibaseDatatype.EXTERNALID description: Online Computer Library Centers kontrollnummer i Worldcat
-    ORCID = "P66"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    PMID = "P68"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    WIKIDATA_QID = "P83"  # datatype: WikibaseDatatype.EXTERNALID description: None
-    ARCHIVE = "P23"  # datatype: WikibaseDatatype.ITEM description: None
-    AUTHOR = "P44"  # datatype: WikibaseDatatype.ITEM description: None
-    CITATIONS = "P51"  # datatype: WikibaseDatatype.ITEM description: None
-    EDITOR = "P47"  # datatype: WikibaseDatatype.ITEM description: None
-    INSTANCE_OF = "P56"  # datatype: WikibaseDatatype.ITEM description: None
-    PUBLISHED_IN = "P211"  # datatype: WikibaseDatatype.ITEM description: None
-    SOURCE_WIKIPEDIA = "P75"  # datatype: WikibaseDatatype.ITEM description: None
-    WEBSITE = "P81"  # datatype: WikibaseDatatype.ITEM description: None
-    SERIES_ORDINAL = "P74"  # datatype: WikibaseDatatype.QUANTITY description: None
-    EDITOR_NAME_STRING = "P48"  # datatype: WikibaseDatatype.STRING description: None
-    FAMILY_NAME = "P49"  # datatype: WikibaseDatatype.STRING description: None
-    FIRST_LEVEL_DOMAIN_STRING = (
-        "P52"  # datatype: WikibaseDatatype.STRING description: None
-    )
-    FULL_NAME_STRING = "P45"  # datatype: WikibaseDatatype.STRING description: None
-    GIVEN_NAME = "P50"  # datatype: WikibaseDatatype.STRING description: None
-    HASH = "P54"  # datatype: WikibaseDatatype.STRING description: None
-    HOST_STRING = "P55"  # datatype: WikibaseDatatype.STRING description: None
-    INTERVIEWER_STRING = "P57"  # datatype: WikibaseDatatype.STRING description: None
-    ISSUE = "P60"  # datatype: WikibaseDatatype.STRING description: None
-    LOCATION_STRING = "P62"  # datatype: WikibaseDatatype.STRING description: None
-    LUMPED_AUTHORS = "P63"  # datatype: WikibaseDatatype.STRING description: None
-    NAME_MASK = "P65"  # datatype: WikibaseDatatype.STRING description: None
-    PAGES = "P67"  # datatype: WikibaseDatatype.STRING description: None
-    PAGE_REVISION_ID = "P73"  # datatype: WikibaseDatatype.STRING description: None
-    PERIODICAL_STRING = "P448"  # datatype: WikibaseDatatype.STRING description: None
-    PUBLISHER_STRING = "P71"  # datatype: WikibaseDatatype.STRING description: None
-    STRING_CITATIONS = "P76"  # datatype: WikibaseDatatype.STRING description: None
-    TEMPLATE_NAME = "P77"  # datatype: WikibaseDatatype.STRING description: None
-    TITLE = "P78"  # datatype: WikibaseDatatype.STRING description: None
-    TRANSLATOR_NAME_STRING = (
-        "P79"  # datatype: WikibaseDatatype.STRING description: None
-    )
-    VOLUME = "P80"  # datatype: WikibaseDatatype.STRING description: None
-    WEBSITE_STRING = "P82"  # datatype: WikibaseDatatype.STRING description: None
-    ACCESS_DATE = "P10"  # datatype: WikibaseDatatype.TIME description: date of access of the resource
-    ARCHIVE_DATE = "P18"  # datatype: WikibaseDatatype.TIME description: None
-    LAST_UPDATE = "P61"  # datatype: WikibaseDatatype.TIME description: date of last update of this item?
-    PUBLICATION_DATE = "P69"  # datatype: WikibaseDatatype.TIME description: date of publication of the resource
-    RETRIEVED_DATE = "P72"  # datatype: WikibaseDatatype.TIME description: date of retrieval of the resource
-    ARCHIVE_URL = "P127"  # datatype: WikibaseDatatype.URL description: None
-    CHAPTER_URL = "P128"  # datatype: WikibaseDatatype.URL description: None
-    CONFERENCE_URL = "P129"  # datatype: WikibaseDatatype.URL description: None
-    FULL_WORK_AVAILABLE_AT_URL = (
-        "P53"  # datatype: WikibaseDatatype.URL description: None
-    )
-    LAY_URL = "P130"  # datatype: WikibaseDatatype.URL description: None
-    TRANSCRIPT_URL = "P131"  # datatype: WikibaseDatatype.URL description: None
-    URL = "P132"  # datatype: WikibaseDatatype.URL description: None
+    DOI = "P1"  # datatype: WikibaseDatatype.EXTERNALID description:
+    GOOGLE_BOOKS_ID = "P2"  # datatype: WikibaseDatatype.EXTERNALID description: Identifier used to link books at Google.
+    INTERNET_ARCHIVE_ID = "P3"  # datatype: WikibaseDatatype.EXTERNALID description: Identifier used to link books and other resources at Internet Archive.
+    ISBN_10 = "P4"  # datatype: WikibaseDatatype.EXTERNALID description:
+    ISBN_13 = "P5"  # datatype: WikibaseDatatype.EXTERNALID description:
+    MEDIAWIKI_PAGE_ID = "P6"  # datatype: WikibaseDatatype.EXTERNALID description:
+    OCLC_CONTROL_NUMBER = "P7"  # datatype: WikibaseDatatype.EXTERNALID description: Online Computer Library Centers control number in Worldcat
+    ORCID = "P8"  # datatype: WikibaseDatatype.EXTERNALID description:
+    PMID = "P9"  # datatype: WikibaseDatatype.EXTERNALID description:
+    WIKIDATA_QID = "P10"  # datatype: WikibaseDatatype.EXTERNALID description:
+    ARCHIVE = "P11"  # datatype: WikibaseDatatype.ITEM description:
+    AUTHOR = "P12"  # datatype: WikibaseDatatype.ITEM description:
+    CITATIONS = "P13"  # datatype: WikibaseDatatype.ITEM description:
+    EDITOR = "P14"  # datatype: WikibaseDatatype.ITEM description:
+    INSTANCE_OF = "P15"  # datatype: WikibaseDatatype.ITEM description:
+    PUBLISHED_IN = "P16"  # datatype: WikibaseDatatype.ITEM description:
+    SOURCE_WIKIPEDIA = "P17"  # datatype: WikibaseDatatype.ITEM description:
+    WEBSITE = "P18"  # datatype: WikibaseDatatype.ITEM description:
+    SERIES_ORDINAL = "P19"  # datatype: WikibaseDatatype.QUANTITY description:
+    EDITOR_NAME_STRING = "P20"  # datatype: WikibaseDatatype.STRING description:
+    FAMILY_NAME = "P21"  # datatype: WikibaseDatatype.STRING description:
+    FIRST_LEVEL_DOMAIN_STRING = "P22"  # datatype: WikibaseDatatype.STRING description:
+    FULL_NAME_STRING = "P23"  # datatype: WikibaseDatatype.STRING description:
+    GIVEN_NAME = "P24"  # datatype: WikibaseDatatype.STRING description:
+    HASH = "P25"  # datatype: WikibaseDatatype.STRING description:
+    HOST_STRING = "P26"  # datatype: WikibaseDatatype.STRING description:
+    INTERVIEWER_STRING = "P27"  # datatype: WikibaseDatatype.STRING description:
+    ISSUE = "P28"  # datatype: WikibaseDatatype.STRING description:
+    LOCATION_STRING = "P29"  # datatype: WikibaseDatatype.STRING description:
+    LUMPED_AUTHORS = "P30"  # datatype: WikibaseDatatype.STRING description:
+    NAME_MASK = "P31"  # datatype: WikibaseDatatype.STRING description:
+    PAGES = "P32"  # datatype: WikibaseDatatype.STRING description:
+    PAGE_REVISION_ID = "P33"  # datatype: WikibaseDatatype.STRING description:
+    PERIODICAL_STRING = "P34"  # datatype: WikibaseDatatype.STRING description:
+    PUBLISHER_STRING = "P35"  # datatype: WikibaseDatatype.STRING description:
+    STRING_CITATIONS = "P36"  # datatype: WikibaseDatatype.STRING description:
+    TEMPLATE_NAME = "P37"  # datatype: WikibaseDatatype.STRING description:
+    TITLE = "P38"  # datatype: WikibaseDatatype.STRING description:
+    TRANSLATOR_NAME_STRING = "P39"  # datatype: WikibaseDatatype.STRING description:
+    VOLUME = "P40"  # datatype: WikibaseDatatype.STRING description:
+    WEBSITE_STRING = "P41"  # datatype: WikibaseDatatype.STRING description:
+    ACCESS_DATE = "P42"  # datatype: WikibaseDatatype.TIME description: date of access of the resource
+    ARCHIVE_DATE = "P43"  # datatype: WikibaseDatatype.TIME description:
+    LAST_UPDATE = "P44"  # datatype: WikibaseDatatype.TIME description: date of last update of this item?
+    PUBLICATION_DATE = "P45"  # datatype: WikibaseDatatype.TIME description: date of publication of the resource
+    RETRIEVED_DATE = "P46"  # datatype: WikibaseDatatype.TIME description: date of retrieval of the resource
+    ARCHIVE_URL = "P47"  # datatype: WikibaseDatatype.URL description:
+    CHAPTER_URL = "P48"  # datatype: WikibaseDatatype.URL description:
+    CONFERENCE_URL = "P49"  # datatype: WikibaseDatatype.URL description:
+    FULL_WORK_AVAILABLE_AT_URL = "P50"  # datatype: WikibaseDatatype.URL description:
+    LAY_URL = "P51"  # datatype: WikibaseDatatype.URL description:
+    TRANSCRIPT_URL = "P52"  # datatype: WikibaseDatatype.URL description:
+    URL = "P53"  # datatype: WikibaseDatatype.URL description:
 
     # Items
-    ENGLISH_WIKIPEDIA = (
-        "Q2"  # label: English Wikipedia description: language version of Wikipedia
-    )
-    WEBSITE_ITEM = "Q32"  # label: Website description: first level domain website found in Wikipedia
-    WIKIPEDIA_PAGE = "Q33"  # label: Wikipedia page description: page in a language version of Wikipedia
-    WIKIPEDIA_REFERENCE = "Q34"  # label: Wikipedia reference description: reference on a page in Wikipedia
-    ARCHIVE_ITEM = "Q6"  # label: Archive description: web archive
-    ARCHIVE_IS = "Q27"  # label: Archive.is description: web archive
-    ARCHIVE_ORG = "Q37"  # label: Archive.org description: web archive
-    ARCHIVE_TODAY = "Q38"  # label: Archive.today description: web archive
-    GHOSTARCHIVE_ORG = "Q2589"  # label: Ghostarchive.org description: web archive
-    WEBCITATION_ORG = "Q39"  # label: Webcitation.org description: web archive
+    ENGLISH_WIKIPEDIA = "Q17"  # label: English Wikipedia description: language version of Wikipedia
+    WEBSITE_ITEM = "Q18"  # label: Website description: first level domain website found in Wikipedia
+    WIKIPEDIA_PAGE = "Q19"  # label: Wikipedia page description: page in a language version of Wikipedia
+    WIKIPEDIA_REFERENCE = "Q20"  # label: Wikipedia reference description: reference on a page in Wikipedia
+    ARCHIVE_ITEM = "Q21"  # label: Archive description: web archive
+    ARCHIVE_IS = "Q22"  # label: Archive.is description: web archive
+    ARCHIVE_ORG = "Q23"  # label: Archive.org description: web archive
+    ARCHIVE_TODAY = "Q24"  # label: Archive.today description: web archive
+    GHOSTARCHIVE_ORG = "Q25"  # label: Ghostarchive.org description: web archive
+    WEBCITATION_ORG = "Q26"  # label: Webcitation.org description: web archive
 
     # This must come last to avoid errors
     wcdqid_language_edition_of_wikipedia_to_work_on = ENGLISH_WIKIPEDIA
