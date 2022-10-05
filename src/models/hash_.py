6d041339e15a61b7f38d9cfc28d61b905fc875d5
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-10-05 14:24:56 +0200

    Add hash_.py

---
+++ src/models/hash_.py
@@ -0,0 +1,31 @@
+import hashlib
+
+from src.models.wikibase import Wikibase
+from src.models.wikibase.ia_sandbox_wikibase import IASandboxWikibase
+from src.models.wikimedia.enums import WikimediaSite
+from src.wcd_base_model import WcdBaseModel
+
+
+class Hash_(WcdBaseModel):
+    wikibase: Wikibase = IASandboxWikibase()
+    language_code: str = "en"
+    wikimedia_site: WikimediaSite = WikimediaSite.WIKIPEDIA
+    title: str = ""
+    article_wikidata_qid: str = ""
+
+    def __entity_updated_hash__(
+        self,
+    ) -> str:
+        """We encode the information we need to make it
+        unique and quick to lookup of the timestamp"""
+        if self.title:
+            title_or_wdqid = self.title
+        else:
+            title_or_wdqid = self.article_wikidata_qid
+        return hashlib.md5(
+            f"{self.wikibase.title}-"
+            f"{title_or_wdqid}-"
+            f"{self.language_code}-"
+            f"{self.wikimedia_site.value}-"
+            f"updated".encode()
+        ).hexdigest()
