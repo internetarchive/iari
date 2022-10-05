a964780cf4776e264f1b46d4bea910a373c0e55f
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-10-05 19:38:58 +0200

    Fix import

--- src/models/message.py
+++ src/models/message.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from typing import Optional
 
-from src import WikimediaSite, Wikibase
+from src.models.wikimedia.enums import WikimediaSite
+from src.models.wikibase import Wikibase
 from src.helpers.console import console
 from src.models.update_delay import UpdateDelay
 from src.models.wikibase.ia_sandbox_wikibase import IASandboxWikibase
