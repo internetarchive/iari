7ab3368cf00fd55b2fc65a7a689d22025ac26644
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-10-08 19:41:35 +0200

    ssdb_database.py: Detect github actions and use redis port

--- src/models/cache/ssdb_database.py
+++ src/models/cache/ssdb_database.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Any, Optional
 
 import pyssdb  # type: ignore
@@ -21,6 +22,11 @@ class SsdbDatabase(WcdBaseModel):
     connection: Optional[Any]
 
     def connect(self):
+        if os.getenv("GITHUB_ACTIONS") == "true":
+            # Use the Redis port for testing
+            self.port = 6379
+        else:
+            raise Exception(f'os.getenv("GITHUB_ACTIONS") was {os.getenv("GITHUB_ACTIONS")}')
         try:
             self.connection = pyssdb.Client(
                 host=self.host,
