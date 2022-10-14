2063ca1c9b3639d5a7c4b1c2a671978be588a2d1
Egil D. Priskorn <egils@riseup.net>
2022-10-14 13:10:23 +0200

    test_work_queue.py: test_publish_test(): Update to changes in model

--- tests/test_work_queue.py
+++ tests/test_work_queue.py
@@ -1,13 +1,15 @@
 import json
 from unittest import TestCase
 
+from src.models.wikibase.ia_sandbox_wikibase import IASandboxWikibase
 from src.models.work_queue import WorkQueue
 
 
 class TestWorkQueue(TestCase):
     def test_publish_test(self):
         w = WorkQueue()
-        w.publish(message=bytes(json.dumps(dict(test="test")), "utf-8"))
+        message = Message(wikibase=IASandboxWikibase(), title="Test")
+        w.publish(message=message)
 
     def test_listen(self):
         w = WorkQueue()
