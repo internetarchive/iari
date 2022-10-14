27c4d4b820ed2e36cdb6a9fb5010bf2b21be53db
Egil D. Priskorn <egils@riseup.net>
2022-10-14 16:31:34 +0200

    wbi.py: Fix pydantic configuration

--- src/helpers/wbi.py
+++ src/helpers/wbi.py
@@ -1,12 +1,12 @@
 from pydantic import validate_arguments
 from wikibaseintegrator.models import Claim  # type: ignore
 
-
-@validate_arguments
+# https://stackoverflow.com/questions/68349150/best-practice-to-pydantic-validate-arguments-for-non-built-in-types-like-pandas
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
 def get_item_value(self, claim: Claim):
     return claim.mainsnak.datavalue["value"]["id"]
 
 
-@validate_arguments
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
 def get_time_value(self, claim: Claim):
     return claim.mainsnak.datavalue["value"]["time"]
