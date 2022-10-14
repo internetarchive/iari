f2db426f36c5c87a1e28dacbf178e509fe795d5b
Egil D. Priskorn <egils@riseup.net>
2022-10-14 09:04:08 +0200

    precommit fixes

--- src/models/wikimedia/wikipedia/reference/english/schema.py
+++ src/models/wikimedia/wikipedia/reference/english/schema.py
@@ -7,6 +7,8 @@ class EnglishWikipediaReferenceSchema(WikipediaReferenceSchema):
     @post_load
     # **kwargs is needed here despite what the validator claims
     def return_object(self, data, **kwargs):  # type: ignore
-        from src.models.wikimedia.wikipedia.reference.english.english_reference import EnglishWikipediaReference
+        from src.models.wikimedia.wikipedia.reference.english.english_reference import (
+            EnglishWikipediaReference,
+        )
 
         return EnglishWikipediaReference(**data)
