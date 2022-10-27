262123d742c63bf47e5a94771cfd00d5f3f7fb3a
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-10-27 18:11:57 +0200

    wbi.py: Fix mypy warnings

--- src/helpers/wbi.py
+++ src/helpers/wbi.py
@@ -4,10 +4,10 @@ from wikibaseintegrator.models import Claim  # type: ignore
 
 # https://stackoverflow.com/questions/68349150/best-practice-to-pydantic-validate-arguments-for-non-built-in-types-like-pandas
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
-def get_item_value(self, claim: Claim):
+def get_item_value(claim: Claim):
     return claim.mainsnak.datavalue["value"]["id"]
 
 
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
-def get_time_value(self, claim: Claim):
+def get_time_value(claim: Claim):
     return claim.mainsnak.datavalue["value"]["time"]
