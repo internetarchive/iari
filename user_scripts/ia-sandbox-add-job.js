146c55910ae31fb718fc7943ec5bc25d4997046d
Egil D. Priskorn <egils@riseup.net>
2022-11-30 12:04:30 +0100

    ia-sandbox-add-job.js: Add namespace check

--- user_scripts/ia-sandbox-add-job.js
+++ user_scripts/ia-sandbox-add-job.js
@@ -2,9 +2,8 @@
 // Author: So9q
 // Date: 2022
 // Inspired by https://en.wikipedia.org/wiki/User:Dipankan001/New_pages.js
-// TODO: add check for namespace == 0 (article)
 
-function addNewpages(){
+function addPortletLink(){
           mw.util.addPortletLink(
           "p-tb",
           "http://18.217.22.248/v1/add-job?lang=en&site=wikipedia&title=" + mw.config.get( 'wgPageName' ) ,
@@ -14,7 +13,9 @@ function addNewpages(){
           );
 }
 
-$(addNewpages);
+if(mw.config.values.wgNamespaceNumber === 0) {
+    $(addPortletLink);
+}
 
 
 // [[Category:Wikipedia scripts]]
