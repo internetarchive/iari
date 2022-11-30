ec2549634e8637c877d19990d42fd251d261a5a4
Egil D. Priskorn <egils@riseup.net>
2022-11-30 12:41:18 +0100

    lookup_by_wikidata_qid.py: Add logging and redirect if match is found for better usability

--- src/models/api/lookup_by_wikidata_qid.py
+++ src/models/api/lookup_by_wikidata_qid.py
@@ -1,19 +1,27 @@
+import logging
 from typing import Union
 
+from flask import redirect
 from flask_restful import Resource  # type: ignore
 
 from src.models.api.enums import Return
 from src.models.api.lookup_wikicitations_qid import LookupWikicitationsQid
+
+logger = logging.getLogger(__name__)
 
 
 class LookupByWikidataQid(Resource):
     @staticmethod
     def get(qid=""):
         if qid:
+            logger.info("Got QID, looking up now")
             lwq = LookupWikicitationsQid()
             result: Union[Return, str] = lwq.lookup_via_query_service(wdqid=qid)
             if isinstance(result, str):
-                return result, 200
+                from src import WikiCitationsWikibase
+
+                wikibase = WikiCitationsWikibase()
+                return redirect(wikibase.entity_url(item_id=result))
             elif result == Return.NO_MATCH:
                 return result.value, 404
             else:
