48c22bb3560bb340105be6fc7d38d03c3f661878
Egil D. Priskorn <egils@riseup.net>
2022-12-01 08:44:54 +0100

    website.py: Improve check for cache

--- src/models/wikibase/website.py
+++ src/models/wikibase/website.py
@@ -20,12 +20,10 @@ class Website(WcdItem):
     @validate_arguments
     def __insert_website_in_cache__(self, wcdqid: str):
         logger.debug("__insert_website_in_cache__: Running")
-        if self.cache is None:
-            self.__setup_cache__()
-        if self.cache is not None:
-            self.cache.add_website(reference=self.reference, wcdqid=wcdqid)
-        else:
+        if not self.cache:
             raise ValueError("self.cache was None")
+        else:
+            self.cache.add_website(reference=self.reference, wcdqid=wcdqid)
         logger.info("Reference inserted into the hash database")
 
     @validate_arguments
@@ -84,14 +82,13 @@ class Website(WcdItem):
     @validate_arguments
     def get_website_wcdqid_from_cache(self) -> None:
         if not self.cache:
-            self.__setup_cache__()
+            raise ValueError("self.cache was None")
         if self.cache:
             self.return_ = self.cache.check_website_and_get_wikibase_qid(
                 reference=self.reference
             )
-            logger.debug(f"result from the cache:{self.return_.item_qid}")
-        else:
-            raise ValueError("self.cache was None")
+            if self.return_:
+                logger.debug(f"result from the cache:{self.return_.item_qid}")
 
     @validate_arguments
     def __upload_website_and_insert_in_the_cache__(
