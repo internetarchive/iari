a93e3f488f77ad17da52b28b2b504b6a592ab683
Egil D. Priskorn <egils@riseup.net>
2022-12-15 09:48:27 +0100

    hashing.py: generate_raw_reference_hash(): New method fixes #429

--- src/models/hashing.py
+++ src/models/hashing.py
@@ -1,5 +1,6 @@
 import hashlib
 
+from src import MissingInformationError
 from src.models.wikibase import Wikibase
 from src.models.wikibase.ia_sandbox_wikibase import IASandboxWikibase
 from src.models.wikimedia.enums import WikimediaSite
@@ -7,11 +8,13 @@ from src.wcd_base_model import WcdBaseModel
 
 
 class Hashing(WcdBaseModel):
+    """Here we handle all the neccesary hashing"""
     wikibase: Wikibase = IASandboxWikibase()
     language_code: str = "en"
     wikimedia_site: WikimediaSite = WikimediaSite.WIKIPEDIA
     title: str = ""
     article_wikidata_qid: str = ""
+    raw_template: str = ""
 
     def __generate_entity_updated_hash_key__(
         self,
@@ -30,4 +33,13 @@ class Hashing(WcdBaseModel):
             f"{self.language_code}-"
             f"{self.wikimedia_site.value}-"
             f"updated".encode()
+        ).hexdigest()
+
+    def generate_raw_reference_hash(self):
+        """Calculate the md5 hash used in the title of the wikipage"""
+        if not self.raw_template:
+            raise MissingInformationError("self.raw_template was empty")
+        # We lower case the whole thing first because we don't care about casing
+        return hashlib.md5(
+            f"{self.raw_template.lower()}".encode()
         ).hexdigest()
