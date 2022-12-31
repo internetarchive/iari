03f25edb6ad1c00ee8a4c264f066ece99906cd78
Dennis Priskorn <egils@riseup.net>
2022-12-31 17:24:38 +0100

    test_get_article_statistics.py: Remove dead code

--- tests/api/test_get_article_statistics.py
+++ tests/api/test_get_article_statistics.py
@@ -1,16 +1,3 @@
-# @pytest.fixture
-# def client():
-#     app = app()
-#     app.config["TESTING"] = True
-#     with app.test_client() as client:
-#         yield client
-#
-#
-# def test_square(client):
-#     rv = client.get("/square?number=8")
-#     assert b"64" == rv.data
-
-
 import unittest
 import json
 
