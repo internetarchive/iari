cd06bf254d3e271ebf2dc55bcada713ef6caab00
Dennis Priskorn <egils@riseup.net>
2023-01-12 12:23:40 +0100

    fix: Failing test 1 modified

--- tests/wikipedia/test_analyzer.py
+++ tests/wikipedia/test_analyzer.py
@@ -153,7 +153,7 @@ class TestWikipediaAnalyzer(TestCase):
             number_of_hashed_content_references=0,
             number_of_isbn_template_references=0,
             number_of_multiple_template_references=0,
-            number_of_named_references=0,
+            number_of_empty_named_references=0,
             number_of_url_template_references=0,
             percent_of_content_references_with_a_hash=0,
             references=[
