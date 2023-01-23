fdd4dc470b56aa936920631ec617bb8a0d92c28f
Dennis Priskorn <egils@riseup.net>
2023-01-23 19:58:29 +0100

    chore: Sort attributes 1 modified

--- src/models/api/get_article_statistics/article_statistics.py
+++ src/models/api/get_article_statistics/article_statistics.py
@@ -12,12 +12,12 @@ class ArticleStatistics(BaseModel):
     We use BaseModel to avoid the cache attribute"""
 
     has_references: bool = False
-    references: Optional[References] = None
-    timing: int = 0  # time to analyze in seconds
-    timestamp: int = 0  # timestamp at beginning of analysis
-    page_id: int = 0  # page id of the Wikipedia in question
     lang: str = "en"  # language code according to Wikimedia
+    page_id: int = 0  # page id of the Wikipedia in question
+    references: Optional[References] = None
     site: str = "wikipedia"  # wikimedia site in question
+    timestamp: int = 0  # timestamp at beginning of analysis
+    timing: int = 0  # time to analyze in seconds
 
     # TODO number_of_images
     # TODO number_of_words
