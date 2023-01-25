b9266b479c47ce752d514afafc81573e5005c6da
Dennis Priskorn <egils@riseup.net>
2023-01-25 19:44:31 +0100

    fix: Fix flask debugging. 3 modified

--- src/models/api/__init__.py
+++ src/models/api/__init__.py
@@ -21,16 +21,14 @@ from src.models.api.lookup_by_wikidata_qid import LookupByWikidataQid
 logging.basicConfig(level=config.loglevel)
 logger = logging.getLogger(__name__)
 
+app = Flask(__name__)
+# We use a prefix here to enable us to stabilize the api over time
+# and bump the version when making breaking changes
+api = Api(app, prefix="/v1")
 
-def create_app():
-    app_ = Flask(__name__)
-    # We use a prefix here to enable us to stabilize the api over time
-    # and bump the version when making breaking changes
-    api = Api(app_, prefix="/v1")
-
-    api.add_resource(LookupByWikidataQid, "/wikidata-qid/<string:qid>")
-    api.add_resource(GetArticleStatistics, "/get-statistics")
-    return app_
-    # api.add_resource(
-    #     AddJobToQueue, "/add-job"
-    # )  # ?lang=<string:language_code>&site=<string:wikimedia_site>&title=<string:title>")
+api.add_resource(LookupByWikidataQid, "/wikidata-qid/<string:qid>")
+api.add_resource(GetArticleStatistics, "/get-statistics")
+# return app_
+# api.add_resource(
+#     AddJobToQueue, "/add-job"
+# )  # ?lang=<string:language_code>&site=<string:wikimedia_site>&title=<string:title>")
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,2 @@
 """This is the entry point for gunicorn"""
-from src.models.api import create_app
-
-app = create_app()
+from src.models.api import app
