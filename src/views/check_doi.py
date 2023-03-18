804843ae1de100ac5a317a217463d86c7bfebb73
Dennis Priskorn <egils@riseup.net>
2023-03-18 17:20:06 +0100

    rewrite: WIP Support refresh and serve cached by default from all, article and the checking endpoints.

--- src/views/check_doi.py
+++ src/views/check_doi.py
@@ -34,23 +34,40 @@ class CheckDoi(StatisticsView):
 
         app.logger.debug("get: running")
         self.__validate_and_get_job__()
-        doi_string = self.job.unquoted_doi
-        app.logger.info(f"Got {doi_string}")
-        doi = Doi(doi=doi_string, timeout=self.job.timeout)
-        doi.lookup_doi()
-        data = doi.get_doi_dictionary()
-        timestamp = datetime.timestamp(datetime.utcnow())
-        data["timestamp"] = int(timestamp)
-        isodate = datetime.isoformat(datetime.utcnow())
-        data["isodate"] = str(isodate)
-        doi_hash_id = self.__doi_hash_id__
-        data["id"] = doi_hash_id
-        write = DoiFileIo(data=data, hash_based_id=doi_hash_id)
-        write.write_to_disk()
-        return data, 200
+        if self.job:
+            return self.__handle_valid_job__()
+
+    def __handle_valid_job__(self):
+        from src.models.api import app
+
+        app.logger.debug("__handle_valid_job__; running")
+
+        self.__read_from_cache__()
+        if self.io.data and not self.job.refresh:
+            return self.io.data, 200
+        else:
+            doi_string = self.job.unquoted_doi
+            app.logger.info(f"Got {doi_string}")
+            doi = Doi(doi=doi_string, timeout=self.job.timeout)
+            doi.lookup_doi()
+            data = doi.get_doi_dictionary()
+            timestamp = datetime.timestamp(datetime.utcnow())
+            data["timestamp"] = int(timestamp)
+            isodate = datetime.isoformat(datetime.utcnow())
+            data["isodate"] = str(isodate)
+            doi_hash_id = self.__doi_hash_id__
+            data["id"] = doi_hash_id
+            write = DoiFileIo(data=data, hash_based_id=doi_hash_id)
+            write.write_to_disk()
+            if self.job.refresh:
+                self.__print_log_message_about_refresh__()
+                data["refreshed_now"] = True
+            else:
+                data["refreshed_now"] = False
+            return data, 200
 
     def __setup_io__(self):
-        pass
+        self.io = DoiFileIo(hash_based_id=self.__doi_hash_id__)
 
     @property
     def __doi_hash_id__(self) -> str:
