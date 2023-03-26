214a638f0bb99a81422b7f8c83a9e901bd0b9cdf
Dennis Priskorn <egils@riseup.net>
2023-03-26 17:10:44 +0200

    chore: Update ReferenceStatistic

--- src/models/api/statistic/reference.py
+++ src/models/api/statistic/reference.py
@@ -5,17 +5,17 @@ from pydantic import BaseModel
 
 class ReferenceStatistic(BaseModel):
     """The purpose of this class is to model the statistics
-    the patron wants from the article endpoint"""
+    the patron wants from the reference endpoint"""
 
     id: str = ""
-    template_names: List[str] = []
+    template_names: List[str]
     wikitext: str
     type: str  # # [general|footnote]
-    footnote_subtype: str = ""  # [named|content]
+    footnote_subtype: str  # [named|content]
     # identifiers: Dict[str, Any]  # {dois: [1234,12345], isbns: [1234]}
     flds: List[str] = []  # non-unique first level domain strings
     urls: List[str] = []  # non-unique url strings
-    templates: List[Dict[str, Any]] = []
+    templates: List[Dict[str, Any]]
     titles: List[str] = []
 
     class Config:  # dead: disable
