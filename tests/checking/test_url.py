fbc2096b387b54fa5c909eb4104c7b28f2b7968d
Dennis Priskorn <egils@riseup.net>
2023-03-29 15:14:11 +0200

    feat: Return also the response headers in check-url. Add tests.

--- tests/checking/test_url.py
+++ tests/checking/test_url.py
@@ -13,19 +13,31 @@ class TestUrl:
         url = Url(url=self.good_url, timeout=2)
         url.check()
         assert url.status_code == 200
+        assert url.response_headers != {}
+        assert url.response_headers["Server"] == "Apache"
 
     def test_check_no(self):
         url = Url(url=self.no_url, timeout=2)
         url.check()
         assert url.status_code == 0
+        assert url.response_headers == {}
 
     def test_check_bad(self):
         url = Url(url=self.bad_url, timeout=2)
         url.check()
         assert url.status_code == 0
         assert url.dns_error is True and url.request_error is True
+        assert url.response_headers == {}
 
     def test_check_403(self):
         url = Url(url=self.forbidden_url_if_not_spoofed_headers, timeout=2)
         url.check()
         assert url.status_code == 200
+        assert url.response_headers["Server"] == "AmazonS3"
+
+    def test_check_response_header(self):
+        url = Url(url=self.good_url, timeout=2)
+        url.check()
+        assert url.status_code == 200
+        assert url.response_headers != {}
+        assert url.response_headers["Server"] == "Apache"
