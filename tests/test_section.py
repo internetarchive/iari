4313772824b0f21d6c2706f8cdca65f7b40ce831
Dennis Priskorn <egils@riseup.net>
2023-04-20 21:04:41 +0200

    fix: Make all tests work for MediawikiSection

--- tests/test_section.py
+++ tests/test_section.py
@@ -1,58 +1,54 @@
 from collections import OrderedDict
 from unittest import TestCase
 
-from mwparserfromhell.wikicode import Wikicode
 from test_data.test_content import (  # type: ignore
     easter_island_tail_excerpt,
     easter_island_head_excerpt,
+    sncaso_tail_excerpt
 )
+
 from src.models.mediawiki.section import MediawikiSection
 from src.models.wikimedia.wikipedia.reference.template.template import WikipediaTemplate
 
 
 class TestMediawikiSection(TestCase):
-    section1 = MediawikiSection(
-        testing=True,
-        wikicode="""==External links==
-{{Commons category|SNCASO}}
-* [http://www.aviafrance.com/constructeur.php?ID_CONSTRUCTEUR=1145 SNCASO page] on [http://www.aviafrance.com AviaFrance].
-
-{{SNCASO aircraft}}
-{{Sud Aviation}}
-{{SNCASO}}
-{{Defunct aircraft manufacturers of France}}
-{{Authority control}}
-
-{{DEFAULTSORT:Sncaso}}
-[[Category:Defunct aircraft manufacturers of France]]
-[[Category:Sud Aviation]]
-[[Category:Vehicle manufacturing companies established in 1936]]
-[[Category:Vehicle manufacturing companies disestablished in 1957]]
-[[Category:French companies established in 1936]]
-[[Category:1957 disestablishments in France]]
-[[Category:Sud-Ouest aircraft| ]] """,
-    )
 
     def test_name(self):
-        assert self.section1.name == "External links"
+        section = MediawikiSection(
+            testing=True,
+            wikicode=sncaso_tail_excerpt,
+        )
+        assert section.name == "External links"
 
     def test_star_found_at_line_start(self):
-        lines = str(self.section1.wikicode).split("\n")
-        assert self.section1.star_found_at_line_start(line=lines[1]) is False
-        assert self.section1.star_found_at_line_start(line=lines[2]) is True
+        section = MediawikiSection(
+            testing=True,
+            wikicode=sncaso_tail_excerpt,
+        )
+        lines = str(section.wikicode).split("\n")
+        assert section.star_found_at_line_start(line=lines[1]) is False
+        assert section.star_found_at_line_start(line=lines[2]) is True
 
     def test_extract(self):
-        self.section1.extract()
-        assert self.section1.number_of_references == 1
-        ref = self.section1.references[0]
+        section = MediawikiSection(
+            testing=True,
+            wikicode=sncaso_tail_excerpt,
+        )
+        section.extract()
+        assert section.number_of_references == 1
+        ref = section.references[0]
         assert ref.templates == list()
         assert ref.reference_id == "ab202d2e"
 
     def test_extract_all_general_references__(self):
-        self.section1.__extract_all_general_references__()
-        print(self.section1.references)
-        assert self.section1.number_of_references == 1
-        ref = self.section1.references[0]
+        section = MediawikiSection(
+            testing=True,
+            wikicode=sncaso_tail_excerpt,
+        )
+        section.__extract_all_general_references__()
+        print(section.references)
+        assert section.number_of_references == 1
+        ref = section.references[0]
         assert ref.templates == list()
         assert ref.reference_id == "ab202d2e"
 
