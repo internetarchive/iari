209e4b708c70fc5bcc8a8ba264615bea1e8c915b
Dennis Priskorn <egils@riseup.net>
2023-05-10 12:20:28 +0200

    fix: Fix path for tests

--- run-test-coverage.sh
+++ run-test-coverage.sh
@@ -1,6 +1,4 @@
 # This change of directory is needed for the test to find the test_data directory
-cd tests/
 poetry run python -m coverage run -m pytest
-cd ..
 poetry run python -m coverage report > TEST_COVERAGE.txt
 echo "TEST_COVERAGE file updated"
--- run-test-find-slow-ones.sh
+++ run-test-find-slow-ones.sh
@@ -1,3 +1,1 @@
-# This change of directory is needed for the test to find the test_data directory
-cd tests/
 poetry run python -m pytest --durations=10
