461724247365486f73afd8aad066c7f5d2d597cd
Dennis Priskorn <egils@riseup.net>
2023-06-06 17:03:43 +0200

    feat: Add more debug output directly from PyMuPDF

--- src/models/api/job/check_url_job.py
+++ src/models/api/job/check_url_job.py
@@ -7,6 +7,10 @@ class UrlJob(Job):
     url: str
     timeout: int = 2  # We default to 2 seconds
     debug: bool = False
+    blocks: bool = False
+    html: bool = False
+    xml: bool = False
+    json_: bool = False
 
     @property
     def unquoted_url(self):
