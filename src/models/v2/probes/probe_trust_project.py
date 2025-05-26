5f5d41fda384b03c7eb4a7f967b0f8f6e048f38e
mojomonger <mojomonger@yahoo.com>
2025-05-25 20:51:10 -0700

    stub for trust_project probe

--- src/models/v2/probes/probe_trust_project.py
+++ src/models/v2/probes/probe_trust_project.py
@@ -1,10 +1,4 @@
-from typing import Any, Dict, List, Optional
-import re
-import logging
-import requests
-
 from src.constants.constants import ProbeMethod
-from src.models.exceptions import MissingInformationError
 
 from src.models.v2.probes.iari_probe import IariProbeBase
 
@@ -13,57 +7,27 @@ class ProbeTrustProject(IariProbeBase):
     """
     "Implements" IariProbeBase class
 
-    logic for obtaining probe results from specific probe
-
-    what we need:
-    link spec // or text later on, for a claim?
-
-    what we return:
-    json formatted probe results
-
+    "probe" method returns json formatted probe results
     """
 
     @property
     def probe_name(self):
-        return ProbeMethod.VERIFYI.value
+        return ProbeMethod.TRUST_PROJECT.value
 
     @staticmethod
     def probe(url):
         """
-        returns results of verifyi probe for url
+        returns results of trust_project probe for url
         """
-
         results = {
             "url": url
         }
 
-        user_agent = "IARI, see https://github.com/internetarchive/iari"
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": user_agent
-        }
-        # TODO do we need to cleanse url here?
-
-        response = requests.post(
-            'https://trustproject.org/',
-            headers=headers,
-            json={'url': url})
-
-        if response.status_code == 200:
-            data = response.json()
-            # TODO do some data transform here before adding results
-            results.update(data)
-
-        else:
-            msg = f"Error probing {url} with {ProbeTrustProject().probe_name}. Got {response.status_code} from {url}"
-            # raise Exception(
-            #     f"Could not probe {self.url}. Got {response.status_code} from {url}"
-            # )
-            results.update({
-                "errors": [
-                    msg
-                ]
-            })
+        results.update({
+            "errors": [
+                "Trust Project probe not yet implemented"
+            ]
+        })
 
         return results
 
